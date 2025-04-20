aff1430754be96d4a41d4d9ceb41ccc6efc43c6c
killerrkittenn <166996539+killerrkittenn@users.noreply.github.com>
2025-04-20 17:23:34 +0500

    Update train_model.py

--- lab/train_model.py
+++ lab/train_model.py
@@ -76,4 +76,7 @@ if __name__ == "__main__":
 
     dfruns = mlflow.search_runs()
     path2model = dfruns.sort_values("metrics.r2", ascending=False).iloc[0]['artifact_uri'].replace("file://","") + '/model' #путь до эксперимента с лучшей моделью
+    with open("best_model.txt", "w") as f:
+        f.write(path2model)
+    print(f"Путь к модели сохранен в best_model.txt: {path2model}")
     print(path2model)
